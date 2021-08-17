720d3a35cc8f18b245f02398799ddcea406634ec
Aluisio Martins Junior <amjr.box@gmail.com>
2021-08-17 13:32:15 -0300

    Início da criação do botão "Evanesco", que permitirá aparecer e desaparecer a div #app.

--- dia1/main.js
+++ dia1/main.js
@@ -1,6 +1,13 @@
-import './style.css'
+import './style.css';
 
 document.querySelector('#app').innerHTML = `
   <h1>Hello Vite!</h1>
   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+`;
+
+const btnLink = document.querySelector('[data-js="btnLink"]');
+const appContainer = document.querySelector('#app');
+
+btnLink.addEventListener('click', () => {
+  appContainer.classList.remove('hide');
+});
