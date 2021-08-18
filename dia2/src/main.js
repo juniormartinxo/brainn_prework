e82b5539a48261bc69fcac406b4f36280ad48eb0
Aluisio Martins Junior <amjr.box@gmail.com>
2021-08-18 09:28:05 -0300

    Alterando o diretório do arquivo main.js e alterando o path no index.html

---
+++ dia2/src/main.js
@@ -0,0 +1,7 @@
+import "../style.css";
+import "./immutable";
+
+document.querySelector("#app").innerHTML = `
+  <h1>Hello Vite!</h1>
+  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
+`;
