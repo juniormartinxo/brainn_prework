9663ea38b64e2a30b061a11ae8beea38b9a11107
Junior Martins <amjr.box@gmail.com>
2021-08-19 14:36:50 -0300

    Solução do exercício 2

--- dia3/src/form.js
+++ dia3/src/form.js
@@ -1,4 +1,7 @@
 const inputName = document.querySelector('[data-js="inputName"]');
+const formContainer = document.querySelector('[data-js="formContainer"]');
+const selectCores = document.createElement('select');
+const boxes = document.querySelector('.boxes');
 
 // valor desse input, fazendo com que, enquanto o valor está sendo digitado, o primeiro caractere de cada palavra seja uma letra maiúscula, a menos que as palavra sejam "de", "da", "do" ou "dos". Todas as outras letras devem ser minúsculas.
 function corrigirNome(str) {
@@ -26,4 +29,29 @@ function corrigirNome(str) {
 
 inputName.addEventListener('keyup', () => {
   inputName.value = corrigirNome(inputName.value);
+});
+
+selectCores.innerHTML = `
+    <option value="">Selecione uma cor</option>
+    <option value="blue">Azul</option>
+    <option value="green">Verde</option>
+    <option value="red">Vermelho</option>
+    <option value="yellow">Amarelo</option>
+    <option value="MidnightBlue">Roxo</option>
+`;
+
+document.body.insertBefore(selectCores, formContainer);
+
+selectCores.addEventListener('change', () => {
+  const boxCor = document.createElement('div');
+  const cor = selectCores.value;
+
+  boxCor.style.backgroundColor = cor;
+  boxCor.style.width = '100px';
+  boxCor.style.height = '100px';
+  boxCor.style.margin = '10px';
+  boxCor.style.borderRadius = '10px';
+  boxCor.style.display = 'inline-block';
+
+  document.body.insertBefore(boxCor, boxes);
 });
