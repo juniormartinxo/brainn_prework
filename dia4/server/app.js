60ffcc585490fce64f96209c9dd21ff8968ce4cc
Junior Martins <amjr.box@gmail.com>
2021-09-08 07:54:39 -0300

    Algumas alterações necessárias

--- dia4/server/app.js
+++ dia4/server/app.js
@@ -1,20 +1,24 @@
-const express = require('express');
-const cors = require('cors');
-const app = express();
-const port = process.env.PORT || 3333;
-const routes = require('./routes');
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const port = process.env.PORT || 3333
+const routes = require('./routes')
 
-app.use(cors());
+app.use(cors())
 
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
 
 app.get('/', (req, res) => {
-  res.json({ message: 'B. Academy Cars API' });
-});
+  res.json({ message: 'B. Academy Cars API' })
+})
 
-app.use('/cars', routes);
+app.delete('/', (req, res) => {
+  res.json({ message: 'B. Academy Cars API' })
+})
+
+app.use('/cars', routes)
 
 app.listen(port, () => {
-  console.log('Listening on port http://localhost:%d', port);
-});
+  console.log('Listening on port http://localhost:%d', port)
+})
--- dia4/server/routes.js
+++ dia4/server/routes.js
@@ -1,29 +1,30 @@
-const express = require('express');
-const router = express.Router();
-const fs = require('fs');
+const express = require('express')
+const router = express.Router()
+const fs = require('fs')
 
-let data = {};
+let data = {}
 
 fs.readFile('./cars.json', 'utf8', function (err, dataJson) {
   if (err) {
-    return console.log('Erro ao ler arquivo ' + err);
+    return console.log('Erro ao ler arquivo ' + err)
   }
 
-  data = JSON.parse(dataJson); // faz o parse para json
-});
+  data = JSON.parse(dataJson) // faz o parse para json
+})
 
 router.get('/', (req, res) => {
-  res.json(Object.values(data));
-});
+  console.log('get')
+  res.json(Object.values(data))
+})
 
 function checkBody(req, res, next) {
   if (areAllFieldsValid(req.body)) {
-    return next();
+    return next()
   }
 
   res
     .status(400)
-    .json({ error: true, message: 'Todos os campos são obrigatórios' });
+    .json({ error: true, message: 'Todos os campos são obrigatórios' })
 }
 
 function areAllFieldsValid(body) {
@@ -35,24 +36,24 @@ function areAllFieldsValid(body) {
     body.year,
     body.plate,
     body.color,
-  ];
-  return fields.every(field => typeof field !== 'undefined' && field !== '');
+  ]
+  return fields.every(field => typeof field !== 'undefined' && field !== '')
 }
 
 function checkAlreadyRegistered(req, res, next) {
-  const infoCar = Object.values(req.body);
-  const dataValores = Object.values(data);
+  const infoCar = Object.values(req.body)
+  const dataValores = Object.values(data)
 
   dataValores.filter(element => {
     if (infoCar[5].toUpperCase() === element.plate.toUpperCase()) {
       return res.status(400).json({
         error: true,
         message: `Já existe um carro cadastrado com a placa ${req.body.plate.toUpperCase()}`,
-      });
+      })
     }
-  });
+  })
 
-  next();
+  next()
 }
 
 router.post('/', checkBody, checkAlreadyRegistered, (req, res) => {
@@ -64,18 +65,19 @@ router.post('/', checkBody, checkAlreadyRegistered, (req, res) => {
     year: req.body.year,
     plate: req.body.plate,
     color: req.body.color,
-  };
+  }
 
   res.json({
     message: `O carro com placa ${req.body.plate} foi cadastrado com sucesso`,
-  });
-});
+  })
+})
 
 router.delete('/', (req, res) => {
-  delete data[req.body.id];
+  console.log('delete')
+  delete data[req.body.id]
   res.json({
     message: `O carro com placa ${req.body.plate} foi removido com sucesso`,
-  });
-});
+  })
+})
 
-module.exports = router;
+module.exports = router
